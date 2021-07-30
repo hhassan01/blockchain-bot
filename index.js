74146cc13e0fd68cfc91a06c0830bfa7823535be
hhassan <hassanhamza0101@gmail.com>
2021-07-31 03:56:30 +0500

    Basic Structure Defined

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-pair.on('Sync', async (reserve0, reserve1) => {
-  console.log(Number(reserve0._hex), '------RESERVE--------', reserve1);
-})
+import { ethers } from 'ethers';
 
-// const pair = await factory.totalSupply();
-// console.log(pair);
+import { account, provider } from './config/wallet.js';
+import { factoryContracts, routerContracts, pairContracts } from './config/contracts.js';
+import { pancakeAddresses, apeAddresses } from './config/addresses.js';
+
+
+const pancakeRouter = new ethers.Contract(pancakeAddresses.router, routerContracts, account);
+const pancakeFactory = new ethers.Contract(pancakeAddresses.factory, factoryContracts, account);
+
+const apeRouter = new ethers.Contract(apeAddresses.router, routerContracts, account);
+const apeFactory = new ethers.Contract(apeAddresses.factory, factoryContracts, account);
+
+// const pair = new ethers.Contract(addresses.wbnbCakePair, pairContracts, account);
+// pair.on('Sync', async (reserve0, reserve1) => {
+//   console.log(Number(reserve0._hex), '------RESERVE--------', reserve1);
+// })
 
 provider.on('block', async (blockNo) => {
-  // Refetch refreshed values of Token prices here. 
-  // console.log(blockNo);
+  console.log(blockNo);
 })
-// Find a method to get total volume of a token (in an exchange?)
+// // Find a method to get total volume of a token (in an exchange?)
